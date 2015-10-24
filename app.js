604b59acc7fc54450e93aaa72b48ddf155c7be66
Daniel Jones <jonesd9@me.com>
2015-10-24 15:44:22 +0100

    GET /receive-sms implemented for clockwork integration

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var logger = require('morgan');
 var routes = require('./routes/index');
-var receiver = require('./routes/receive-sms.js');
+var receiver = require('./routes/receive-sms');
 
 var app = express();
 
@@ -15,7 +11,6 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.use('/', routes);
 app.use('/receive-sms', receiver);
--- routes/receive-sms.js
+++ routes/receive-sms.js
@@ -2,14 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* Receive SMS */
-router.post('', function(req, res) {
-  res.json({ status: "received", sms : {
-    to : req.query.to,
-    from : req.query.from,
-    message : req.query.content,
-    keyword : req.query.keyword,
-    msg_id : req.query.msg_id
-  }});
+router.get('/', function(req, res) {
+  res.json({ status: "received",
+    sms : {
+      to : req.query.to,
+      from : req.query.from,
+      message : req.query.content,
+      keyword : req.query.keyword,
+      msg_id : req.query.msg_id
+    }
+  });
 });
 
 module.exports = router;
