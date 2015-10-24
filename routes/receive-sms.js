9459810e0a872d27736e9fc0e2739338355d5a0c
Daniel Jones <jonesd9@me.com>
2015-10-24 22:45:39 +0100

    Added capability to call logic!!!!!!!!!!!!

--- routes/receive-sms.js
+++ routes/receive-sms.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
+
+var logic_url_base = "http://ec2-52-17-7-182.eu-west-1.compute.amazonaws.com"
 
 /* Receive SMS */
 router.get('/', function(req, res) {
+
+  var options = {
+    hostname: logic_url_base,
+    port: 5000,
+    path: '/logic',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': postData.length
+    }
+  };
+
+  http.request(options, function(res) {
+    console.log("request to LOGIC completed: " + res);
+  });
+
   res.json({ status: "received",
     sms : {
       to : req.query.to,
